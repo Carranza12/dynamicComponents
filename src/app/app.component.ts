8df6d5c6fa8329849b591ab290b5df3607d078c6
Francisco Carranza <Carranza12>
2023-01-21 19:37:50 -0600

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DynamicHostDirective } from './directives/dynamic-host.directive';
+import { DynamicComponent } from './dynamic/dynamic.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dynamic-component';
+  @ViewChild (DynamicHostDirective) public dynamicHost!: DynamicHostDirective;
+
+  constructor(private _componentFactoryResolver: ComponentFactoryResolver){}
+
+  public createComponent():void{
+    const component = this._componentFactoryResolver.resolveComponentFactory(DynamicComponent);
+    this.dynamicHost._viewContainerRef.createComponent(component);
+  }
+
+  public deleteComponents():void{
+    this.dynamicHost._viewContainerRef.clear();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DynamicHostDirective } from './directives/dynamic-host.directive';
+import { DynamicComponent } from './dynamic/dynamic.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DynamicHostDirective,
+    DynamicComponent,
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
